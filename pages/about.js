1aed91c1b899acfc0e1ab8a06c29e57875952e16
salah deco <salah.deco1@gmail.com>
2022-08-18 10:54:15 +0100

    Pages & Routes, adding other Components

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return ( 
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo porro ipsum ipsa est, vitae accusantium culpa nemo animi numquam eligendi modi doloremque voluptatem sint iusto perferendis nostrum, quidem vel ut nobis officiis error quibusdam dignissimos ratione cupiditate? Consequuntur accusamus libero doloribus voluptatem nisi expedita, voluptatum sapiente recusandae velit totam maiores fuga quo nemo labore commodi sunt, cum blanditiis repellat nobis, atque maxime iusto sit. Error a illum tempore optio esse repellendus hic totam, laboriosam quo sed sequi laudantium ea, dicta debitis quibusdam enim rem dolorum cupiditate nulla doloremque consequatur earum. Debitis at aliquid natus soluta explicabo consequatur illum, ab fugit.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis corrupti architecto ut reprehenderit quibusdam autem sed maiores. Atque autem provident fuga totam ipsum. Asperiores ipsa, quasi ipsam dignissimos magni corporis sequi possimus aperiam a, eaque quas totam, qui et quibusdam sit voluptates tempora aliquid! Accusantium quae explicabo quidem assumenda ipsa. Animi vitae omnis nostrum harum cupiditate deleniti perferendis pariatur ipsam aut ab ea odit doloribus cumque similique maxime voluptates, itaque dolor quibusdam ut necessitatibus laboriosam enim, voluptate eos! Voluptates sed cumque nam cum atque nisi!</p>
+        </div>
+    );
+}
+ 
+export default About;
