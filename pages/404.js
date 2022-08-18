855ed90bb67df6fdc9a93c27c82f3fc787ddb755
salah deco <salah.deco1@gmail.com>
2022-08-18 12:38:08 +0100

    Redirecting Users

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,19 @@
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 const NotFound = () => {
+    const router = useRouter();
+
+    useEffect(() => {
+        console.log('use effect run');
+        setTimeout(() => {
+            router.push('/');
+        }, 3000);
+    }, [])
+
     return ( 
         <div className="container">
             <div className="not-found">
